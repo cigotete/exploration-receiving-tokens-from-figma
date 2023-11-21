1d30c85ef1258270060f6d1db0a116a459f56592
Camilo Figueroa <cigotete@gmail.com>
2023-11-21 16:16:36 -0500

    Moving aditional components tokens outside tokens json file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import {ConfigProvider, Space, Button} from 'antd';
-
 import tokens from './tokens.json';
 
 export const App = () => {
 
+  const additionalComponents = {
+    "components": {
+      "Layout": {
+        "colorBgHeader": "#ffffff"
+      }
+    }
+  };
+  
+  // Merge the initial JSON object with the additional components
+  const mergedStyleDefinition = { ...tokens, ...additionalComponents };
+
   return (
     <ConfigProvider
-    theme={tokens}
+    theme={mergedStyleDefinition}
   >
     <Space>
       <Button type="primary">Primary</Button>
